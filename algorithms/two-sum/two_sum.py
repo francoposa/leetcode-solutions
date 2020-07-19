5fb58b665b737c3e783e628bbb72c12faf9f3267
francojposa <franco@francoposa.io>
2020-07-19 09:47:54 -0500

    update two sum to handle case where candidate num is half of target bnut only appears once

--- algorithms/two-sum/two_sum.py
+++ algorithms/two-sum/two_sum.py
@@ -8,14 +8,28 @@ class Solution:
                 continue
             for j, num2 in enumerate(nums):
                 if num1 + num2 == target:
-                    return [i, j]
+                    return i, j
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_indices = {num: i for i, num in enumerate(nums)}
+        num_indices = {num: [] for num in nums}
+        for i, num in enumerate(nums):
+            num_indices[num].append(i)
+
         diffs = [target - num for num in nums]
-        for diff in diffs:
-            if diff in num_indices:
-                return [
-                    num_indices[target - diff],
-                    num_indices[diff],
-                ]
+        for diff_num in diffs:
+            diff_num_indices = num_indices.get(diff_num)
+            other_num = target - diff_num
+            other_num_indices = num_indices.get(other_num)
+            if all((diff_num_indices, other_num_indices)):
+                if diff_num == other_num:
+                    # both candidate nums are half the target sum.
+                    # we need to check that the num actually occurred twice
+                    # in the original array before declaring success
+                    if len(diff_num_indices) > 1:
+                        return diff_num_indices
+                    continue  # false positive, the candidate num only appeared once
+                else:
+                    return [
+                        other_num_indices[0],
+                        diff_num_indices[0],
+                    ]
